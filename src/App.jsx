18eaaa6b3ddfbc5b48365bf9e752325381dc0169
Roman Ryzhakov <ryzhakov.r@mail.ru>
2018-03-27 23:41:02 +0300

    added dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// export default () => console.log('App', React);
+
+export default class App extends React.Component {
+    render() {
+        const name = 'App';
+
+        return (
+            <h1>{name}</h1>
+        )
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                use: ["babel-loader"]
+            }
+
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            React: 'react',
+        })
+    ]
 };
